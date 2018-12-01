d0feb57817a46b93242be6b2fd1ef44d842d13ee
callumg2895 <callumgoodin@gmail.com>
2018-12-01 18:12:02 +0000

    Slight refactor of 2018 day 1 solutions.

--- 2018 Solutions/day_1_solutions.py
+++ 2018 Solutions/day_1_solutions.py
@@ -1,39 +1,31 @@
 import csv
 
 #################################################################
-## Functions ##
+######################### Functions #############################
 
-def get_total(input_file_name):
-    
+def get_input(input_file_name):
+    with open(input_file_name, newline = '') as input_file:
+        return [int(row) for row in input_file]
+
+def get_total(input):
+    return sum([i for i in input])
+
+def get_first_repeat(input):
+
+    result_set = set({})
     result = 0
 
-    with open(input_file_name, newline = '') as input_file:
-        for row in input_file:
+    while True:
+        for row in input:            
             result += int(row)
 
-    return result
+            if result in result_set: 
+                return result  
 
-def get_first_repeat(input_file_name):
-
-    result_dict = {}
-    result = 0
-    finished = False
-
-    while not finished:
-        with open(input_file_name, newline = '') as input_file:
-            for row in input_file:
-                
-                result += int(row)
-
-                if result in result_dict:
-                    finished = True
-                    break
-                else:
-                    result_dict[result] = 0
-    
-    return result
+            result_set.add(result)
 
 #################################################################
 
-print(get_total('day_1_input.csv'))
-print(get_first_repeat('day_1_input.csv'))
+input = get_input('day_1_input.csv')
+print(get_total(input))
+print(get_first_repeat(input))
